5e092766537af8b063af4551eadb025032d7951f
lolimay <1404363070@qq.com>
2019-08-01 20:41:27 +0800

    add people style

--- src/containers/MarkdownLoader.js
+++ src/containers/MarkdownLoader.js
@@ -40,6 +40,27 @@ const SMarkdown = styled.div`
     h1, h2, h3, h4, h5, h6 {
         font-family: "Charter", "Bitstream Charter", Georgia, serif;
     }
+    .circle-border {
+        position: relative;
+        width: 130px;
+        height: 130px;
+        display: inline-block;
+        ::after {
+            position: absolute;
+            top: 0;
+            left: 0;
+            box-shadow: 0 0 0 5px rgba(255, 255, 255, 0.6) inset, 0 1px 2px rgba(0, 0, 0, 0.3);
+            content: "";
+            width: 135px;
+            height: 135px;
+            border-radius: 50%;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+    }
     a {
         color: #0088cc;
         text-decoration: none;
